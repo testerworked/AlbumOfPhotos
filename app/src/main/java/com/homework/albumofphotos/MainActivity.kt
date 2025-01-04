07511a881a91f4cfe84b216e18e46a1dea740cac
Rinat Khuzin <gipnoz.rinat@gmail.com>
2025-01-04 13:29:24 +0500

    add: gallery functions;

--- app/src/main/java/com/homework/albumofphotos/MainActivity.kt
+++ app/src/main/java/com/homework/albumofphotos/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.homework.albumofphotos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val startButton: Button = findViewById(R.id.buttonStart)
+        startButton.setOnClickListener {
+            val intent = Intent(this, PhotoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/homework/albumofphotos/PhotoActivity.kt
@@ -0,0 +1,53 @@
+package com.homework.albumofphotos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageView
+
+
+
+class PhotoActivity : AppCompatActivity() {
+
+    private val photos = arrayOf(
+        R.drawable.photo1,
+        R.drawable.photo2,
+        R.drawable.photo3,
+        R.drawable.photo4,
+        R.drawable.photo5
+    )
+
+    private var currentPhotoIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_photo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val imageView: ImageView = findViewById(R.id.imageViewPhoto)
+        val nextPhotoButton: Button = findViewById(R.id.buttonNextPhoto)
+
+        imageView.setImageResource(photos[currentPhotoIndex])
+
+        nextPhotoButton.setOnClickListener {
+            if (currentPhotoIndex < photos.size - 1) {
+                currentPhotoIndex++
+                imageView.setImageResource(photos[currentPhotoIndex])
+            } else {
+                val intent = Intent(this, FinalActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
